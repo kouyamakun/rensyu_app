817db83c5b30cc21d3bf07e4a88e9d45cc6d8c04
ruby-v <guruguru230203@gmil.com>
2015-05-03 14:50:45 +0900

    一意性の確認

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+  #name,emailは入力必須
+  validates(:name, presence: true, length: { maximum: 50 })
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                uniqueness: { case_sensitive: false }
+end
